a0895a9654eab7f785854551519adac5f445f00c
Shaun Dawson <iamsdawson@gmail.com>
2021-03-14 19:00:25 -0400

    validate guess, set messages

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ GAME FUNCTION:
  */
 
  // Games values
- let min = 5, 
+ let min = 1, 
      max = 10,
      winningNum =  2, 
      guessesLeft = 3;
@@ -19,8 +19,37 @@ const game = document.querySelector('#game'),
       maxNum = document.querySelector('.max-num'),
       guessBtn = document.querySelector('#guess-btn'),
       guessInput = document.querySelector('#guess-input'),
-      message = document.querySelector('#message');
+      message = document.querySelector('.message');
 
 // Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
+
+// Listen for guess
+guessBtn.addEventListener('click', function(){
+  let guess = parseInt(guessInput.value);
+
+  // Validate
+  if( isNaN(guess)|| guess < min || guess > max){
+    setMessage(`Please enter a number between ${min} and ${max}`,'red');
+  }
+
+  // Check if won
+  if(guess === winningNum){
+    // Disable input
+    guessInput.disabled = true;
+    // Change border color
+    guessInput.style.borderColor='green';
+    // Set message
+    setMessage(`${winningNum} is correct, YOU WIN!`,'green');
+  } else {
+  }
+
+});
+
+// Set message
+function setMessage(msg, color){
+  message.style.color= color;
+  message.textContent = msg;
+}
+
